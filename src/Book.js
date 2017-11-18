afdc7c8680ce4640270fbde1f9b1c0a47a3319fa
Carlos Loureda <carlos.loureda@domesting.com>
2017-11-18 16:23:48 +0100

    fix: Added default selection on main page shelf selection dropdown

--- src/Book.js
+++ src/Book.js
@@ -10,13 +10,14 @@ class Book extends Component {
     render() {
         const book = this.props.book;
         const coverUrl = this.props.book.imageLinks.smallThumbnail;
-
+        let defaultShelf = this.props.book.shelf;
+        console.log("THe book is: ", book);
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${coverUrl}"` }}></div>
+                    <div className="book-cover" style={{width: 128, height: 192, backgroundImage: `url("${coverUrl}"` }}></div>
                     <div className="book-shelf-changer">
-                        <select onClick={(event) => this.props.onChangeShelf(event, book)} >
+                        <select onClick={(event) => this.props.onChangeShelf(event, book)} defaultValue={defaultShelf}>
                             <option value="none" disabled>Move to...</option>
                             <option name="currentlyReading" value="currentlyReading">Currently Reading</option>
                             <option name="wantToRead" value="wantToRead">Want to Read</option>
